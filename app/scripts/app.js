c06574f41ad915b5e2a7eebb3095b6728265853f
Miles Thompson <utunga@gmail.com>
2013-04-21 18:43:20 +1200

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angpockApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
